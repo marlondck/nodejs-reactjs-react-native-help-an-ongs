1981ef1fb6930c51167ba779b37731c5a800ed5a
marlondck <15050029+marlondck@users.noreply.github.com>
2021-03-09 21:46:06 -0400

    cadastro da ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'ola mundo' });
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+
+   await connection('ongs').insert({
+    id, name, email, whatsapp, city, uf
+  })
+
+  return res.json({ id });
 })
 
 module.exports = routes;
