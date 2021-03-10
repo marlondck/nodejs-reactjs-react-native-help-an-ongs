7e6a55c5675d91371ed31608e5a6ec70e47e8f0e
marlondck <15050029+marlondck@users.noreply.github.com>
2021-03-10 16:46:24 -0400

    paginas e estilizacoes

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -19,10 +19,10 @@ function Logon() {
           <input type="text" placeholder="Sua ID"/>
           <button className="button"type="submit">Entrar</button>
 
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
 
