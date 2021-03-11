0a8e42a33770f75f112b43408dc769dd1bf79877
marlondck <15050029+marlondck@users.noreply.github.com>
2021-03-11 16:07:23 -0400

    scroll infinito

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -10,8 +10,10 @@ import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 function Incidents () {
-  const [incidents, setIncidents] = useState([])
-  const [totalIncidents, setTotalIncidents] = useState(0)
+  const [incidents, setIncidents] = useState([]);
+  const [totalIncidents, setTotalIncidents] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
@@ -20,9 +22,25 @@ function Incidents () {
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
-    setIncidents(response.data);
-    setTotalIncidents(response.headers['x-total-count'])
+    if(loading === true) { 
+      return;
+    }
+    
+    if(totalIncidents > 0 && incidents.length === totalIncidents) { 
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...response.data]);
+    setTotalIncidents(response.headers['x-total-count']);
+    
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -46,6 +64,8 @@ function Incidents () {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incidentCard}>
           <Text style={styles.incidentProperty}>ONG:</Text>
